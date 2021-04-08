35153ff154d54ab55a85291d436d37befb995447
NiazMahmud <s.m.mahmud@northsouth.edu>
2021-04-08 09:28:55 +0600

    1st update

---
+++ Watch1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Watch1 : MonoBehaviour
+{
+    public GameObject watchm1;
+    public GameObject watchm2;
+    
+    public GameObject ww1;
+    public GameObject ww2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+}
